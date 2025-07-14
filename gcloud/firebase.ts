96ced3a286661052557d2ecf9beb4609f4bda7fb
Peter Stark <peterstark72@gmail.com>
2025-07-14 12:29:38 +0200

    added firebase

---
+++ gcloud/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDWzIvm-VW4WnEM_ERFfD1ikJJp0wdCqJk",
+  authDomain: "tygelsjo-se.firebaseapp.com",
+  projectId: "tygelsjo-se",
+  storageBucket: "tygelsjo-se.firebasestorage.app",
+  messagingSenderId: "297196254846",
+  appId: "1:297196254846:web:258dc58c6d8a72a2338a2f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
