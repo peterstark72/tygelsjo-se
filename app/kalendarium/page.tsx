d051bbd644376195aac08cf22a9bead96d25935b
Peter Stark <peterstark72@gmail.com>
2025-07-27 17:17:20 +0200

    fixed date for events

--- app/kalendarium/page.tsx
+++ app/kalendarium/page.tsx
@@ -4,8 +4,14 @@ import { client } from '@/sanity/lib/client';
 
 import styles from './page.module.css';
 
+export const revalidate = 3600 // 1 hour
 
-const seLocale = new Intl.Locale("sv-SE")
+const dateFormatOptions : Intl.DateTimeFormatOptions = {
+  dateStyle: "full",
+  timeStyle: "short"
+};
+
+const dateTimeFormat: Intl.DateTimeFormat = new Intl.DateTimeFormat("sv-SE", dateFormatOptions)
 
 export default async function Page() {
 
@@ -15,11 +21,11 @@ export default async function Page() {
     return (
         <div className={styles.page}>
             <h1>Kalendarium</h1>
-            <p>This is the Kalendarium page.</p>
+            <p>Händelser i Tygelsjö och V Klagstorp.</p>
             <ul className={styles.events}>
                 {events.map((event) => (
                     <li key={event._id}>
-                        <p>{new Date(event.start).toLocaleString(seLocale, {timeStyle: "short", dateStyle: "full"})} - {new Date(event.end).toLocaleTimeString(seLocale, {timeStyle: "short"})}</p>
+                        <p>{event.end != null ? dateTimeFormat.formatRange(new Date(event.start), new Date(event.end)) : dateTimeFormat.format(new Date(event.start))}</p>
                         <h2>{event.name}</h2>
                         <p>{event.place.name}</p>
                         <p>{event.description}</p>
